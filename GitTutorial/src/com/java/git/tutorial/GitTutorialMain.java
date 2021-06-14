53b4c742e62921780858707bfc71b4fd9c316516
ssaw1986 <alanine121@gmail.com>
2021-06-14 16:21:20 -0700

    Commit V1.1

--- GitTutorial/src/com/java/git/tutorial/GitTutorialMain.java
+++ GitTutorial/src/com/java/git/tutorial/GitTutorialMain.java
@@ -5,6 +5,7 @@ public class GitTutorialMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(GitTutorials.returnData());
+		System.out.println("Added this syso to have second version in git");
 	}
 
 }
