03211b2e3d052724f9bad2cccfc238bdd1385020
ssaw1986 <alanine121@gmail.com>
2021-06-14 15:29:00 -0700

    java program git commit v1.0

---
+++ GitTutorial/src/com/java/git/tutorial/GitTutorialMain.java
@@ -0,0 +1,10 @@
+package com.java.git.tutorial;
+
+public class GitTutorialMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(GitTutorials.returnData());
+	}
+
+}
---
+++ GitTutorial/src/com/java/git/tutorial/GitTutorials.java
@@ -0,0 +1,10 @@
+package com.java.git.tutorial;
+
+public class GitTutorials {
+
+	public static int data = 100;
+	public static int returnData() {
+		return data;
+	}
+	
+}
